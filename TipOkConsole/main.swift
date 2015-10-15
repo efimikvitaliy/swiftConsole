e6bf8963499db5f1f096c829dcf0bec7c0cbb510
efimikvitaliy <efimikvitaliy@Mac-Admin.local>
2015-10-15 15:32:51 +0300

    Initial Commit

---
+++ TipOkConsole/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TipOkConsole
+//
+//  Created by efimikvitaliy on 15.10.15.
+//  Copyright (c) 2015 DreamTeam. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
